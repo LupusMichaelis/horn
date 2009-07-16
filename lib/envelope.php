8b5dc1e68f6f5cfae8f4f84f38e6e4717f9f178c
Lupus Michaelis <mickael@lupusmic.org>
2009-07-16 10:27:40 +0200

    Integrate envelope and observable concepts.

---
+++ lib/envelope.php
@@ -0,0 +1,76 @@
+<?php
+/** Envelope pattern.
+ *
+ *  Project	Horn Framework <http://horn.lupusmic.org>
+ *  \author		Lupus Michaelis <mickael@lupusmic.org>
+ *  Copyright	2009, Lupus Michaelis
+ *  License	AGPL <http://www.fsf.org/licensing/licenses/agpl-3.0.html>
+ */
+
+/*
+ *  This file is part of Horn Framework.
+ *
+ *  Horn Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Horn Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero Public License
+ *  along with Horn Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/** \package	horn
+ */
+namespace horn ;
+
+require_once 'horn/lib/object.php' ;
+
+/**
+ */
+class envelope
+	extends object_public
+{
+	/**
+	 */
+	public		function __construct(object_base $content)
+	{
+		parent::__construct() ;
+		$this->attach($content) ;
+	}
+
+	/**
+	 */
+	final
+	public		function __call($method, $args)
+	{
+		if(method_exists($this->_content, $method))
+			call_user_method_array($method, $this->_content, $args) ;
+		else
+			$this->_throw_unknown_method() ;
+	}
+
+	/**
+	 */
+	public		function attach(object_base $content)
+	{
+		$this->_content = $content ;
+	}
+
+	/** 
+	 *	\see parent::_set
+	 */
+	protected	function _set($name, $value)
+	{
+		$this->_content->_set($name, $value) ;
+	}
+
+	protected	$content ;
+}
+
+
---
+++ lib/observable.php
@@ -0,0 +1,151 @@
+<?php
+/** observable class definition
+ *
+ *  Project	Horn Framework <http://horn.lupusmic.org>
+ *  \author		Lupus Michaelis <mickael@lupusmic.org>
+ *  Copyright	2009, Lupus Michaelis
+ *  License	AGPL <http://www.fsf.org/licensing/licenses/agpl-3.0.html>
+ */
+
+/*
+ *  This file is part of Horn Framework.
+ *
+ *  Horn Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Horn Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero Public License
+ *  along with Horn Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace horn ;
+
+require_once 'horn/lib/object.php' ;
+require_once 'horn/lib/collection.php' ;
+
+/**
+ */
+class callback
+	extends object_public
+{
+	/**
+	 */
+	public		function __construct($native)
+	{
+		parent::__construct() ;
+		$this->native = $native ;
+	}
+
+	/**
+	 */
+	public		function __invoke()
+	{
+		return call_user_func_array($this->native, func_get_args()) ;
+	}
+
+	protected	function _set_native($native)
+	{
+		if(!is_callable($native))
+			throw new exception
+				(sprintf('Bad callback named \'%s\' supplied.', $native)) ;
+
+		$this->_native = $native ;
+	}
+
+	/**
+	 */
+	protected	$_native ;
+}
+
+/** Observable interface.
+ *
+ */
+interface iObservable
+{
+	/** Notify listener that the $event happened.
+	function	notify($event) ;
+	 */
+
+	/** Register a listener callback.
+	 */
+	function	register(callback $callback, $event = null) ;
+
+	/** On destruction, listener must be notified.
+	function	__destruct() ;
+	 */
+}
+
+/** Observable object.
+ *
+ */
+class observable
+	extends		object_public
+	implements	iObservable
+{
+	/** 
+	 */
+	public		function __construct()
+	{
+		parent::__construct() ;
+		$this->callbacks = new collection ;
+	}
+
+	protected	function _add_event($event)
+	{
+		$this->callbacks[$event] = new collection ;
+	}
+
+	/** Register a listener callback.
+	 */
+	public		function register(callback $callback, $event = null)
+	{
+		if(is_null($event))
+			foreach($this->callbacks as $event => $callbacks)
+				$this->callbacks[$event][] = $callback ;
+		elseif(isset($this->_callbacks[$event]))
+			$this->callbacks[$event][] = $callback ;
+		else
+			throw new exception("Unhandled event '$event'") ;
+	}
+
+	/** Unregister a listener callback.
+	 */
+	public		function unregister(callback $callback, $event = null)
+	{
+		if(is_null($event))
+			foreach($this->callbacks as $event => $callbacks)
+				$this->callbacks[$event]->remove($callback) ;
+		elseif(isset($this->_callbacks[$event]))
+			$this->callbacks[$event]->remove($callback) ;
+		else
+			throw new exception("Unhandled event '$event'") ;
+	}
+
+	/** Notify listener that the $event happened.
+	 */
+	protected	function _notify($event)
+	{
+		$data = null ; // \todo
+
+		if($this->callbacks[$event]->count())
+			foreach($this->callbacks[$event] as $callback)
+				$callback($event, $data) ;
+	}
+
+	/** Stack of callbacks.
+	 */
+	protected	$_callbacks ;
+}
+
+
+
+
+
+
