056cda5779ec713cca85f8d30cf089b218a0bd9a
Mickaël Wolff <mickael@lupusmic.org>
2011-01-23 03:07:11 +0000

    HTTP request class

---
+++ lib/http_request.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace horn\lib\http ;
+
+require_once 'horn/lib/collection.php' ;
+
+class request
+{
+	public	$header ;
+	public	$body ;
+
+	static $c_methods = array
+		( 'POST' => self::POST
+		, 'GET' => self::GET
+		, 'PUT' => self::PUT
+		, 'DELETE' => self::DELETE
+		) ;
+
+	const	POST = 'POST' ;
+	const	GET = 'GET' ;
+	const	PUT = 'PUT' ;
+	const	DELETE = 'DELETE' ;
+
+	public function __construct()
+	{
+		$this->header = new header ;
+		$this->body = new body ;
+	}
+
+	static public function create_native()
+	{
+		$native = new self ;
+		$native->header['host'] = $_SERVER['HTTP_HOST'] ;
+
+		$native->request->method = self::$c_methods[$_SERVER['REQUEST_METHOD']] ;
+		$native->request->uri = $_SERVER['REQUEST_URI'] ;
+		$native->request->version = $_SERVER['SERVER_PROTOCOL'] ;
+
+		return $native ;
+	}
+}
+
+class header
+	extends \horn\collection
+{
+}
+
+class body
+{
+	public	$content ;
+}
+
+
+
