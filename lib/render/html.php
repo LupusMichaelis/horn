6ce624c63ee4f2e42345a1da6fcada2219931945
Mickaël Wolff <mickael@lupusmic.org>
2011-02-10 12:24:42 +0000

    HTML render: add title helper

--- lib/render/html.php
+++ lib/render/html.php
@@ -8,11 +8,24 @@ class html
 	extends object_public
 {
 	protected	$_document ;
+	protected	$_title ;
 
 	public		function __construct()
 	{
 		$template = '<html><head><title></title></head><body></body></html>' ;
 		$this->_document = \DomDocument::loadHTML($template) ;
+	}
+
+	protected	function _set_title($text)
+	{
+		$this->_title = $text ;
+		$titleElement = $this->document->getElementsByTagName('title')->item(0) ;
+		$newTitleTxt = $this->document->createTextNode($text) ;
+		$oldTitleTxt = $titleElement->childNodes->item(0) ;
+		if($oldTitleTxt)
+			$titleElement->replaceChild($newTitleTxt, $oldTitleTxt) ;
+		else
+			$titleElement->appendChild($newTitleTxt) ;
 	}
 
 	public		function __tostring()
