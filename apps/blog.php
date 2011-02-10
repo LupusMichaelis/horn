2c5d9b115c50a67866d67cfab6d341f16527235e
Mickaël Wolff <mickael@lupusmic.org>
2011-02-10 12:54:20 +0000

    Blog app: model for post

--- apps/blog.php
+++ apps/blog.php
@@ -3,14 +3,21 @@
 namespace horn\apps ;
 use \horn\lib as h ;
 
+require_once 'horn/lib/collection.php' ;
+
 require_once 'horn/lib/app.php' ;
 require_once 'horn/lib/render/html.php' ;
 
 class blog
 	extends \horn\lib\app
 {
+	protected	$_posts ;
+
 	public		function run()
 	{
+		$this->posts = new h\collection ;
+		$this->posts->push(post::create('First post', 'This is a very first post')) ;
+
 		$this->prepare_renderer() ;
 		return $this ;
 	}
@@ -33,3 +40,24 @@ class blog
 		//$this->response->set_content_type($type, 'utf-8') ;
 	}
 }
+
+class post
+	extends h\object_public
+{
+	public		$title ;
+	public		$description ;
+	public		$created ;
+	public		$modified ;
+
+	// public	$owner ;
+
+	static
+	public		function create($title, $description)
+	{
+		$new = new static ;
+		$new->title = $title ;
+		$new->description = $description ;
+		return $new ;
+	}
+}
+
