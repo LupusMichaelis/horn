e3eb1fdf1fb1e100f3c6d583a9172a5d0b73530a
Mickaël Wolff <mickael@lupusmic.org>
2011-02-14 02:08:17 +0000

    Some reordering

--- examples/blog.php
+++ examples/blog.php
@@ -3,11 +3,11 @@
 require_once 'horn/apps/blog.php' ;
 require_once 'horn/lib/router.php' ;
 
-$in = \horn\lib\http\request::create_native() ;
-$out = new \horn\lib\http\response ;
-
 // Everything is routed to info application
 $routing = array('\horn\apps\blog') ;
+
+$in = \horn\lib\http\request::create_native() ;
+$out = new \horn\lib\http\response ;
 
 $main = \horn\lib\run($in, $out, $routing) ;
 $main->run() ;
