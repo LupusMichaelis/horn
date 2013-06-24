87c6a0e4d51a7c53436aff154b7c03c9a1802a02
Mickaël Wolff <mickael@lupusmic.org>
2013-06-24 09:44:03 +0100

    Add decorator concept

---
+++ lib/decorator.php
@@ -0,0 +1,58 @@
+<?php
+/** Decorator design pattern
+ *
+ *  Project	Horn Framework <http://horn.lupusmic.org>
+ *  \author		Lupus Michaelis <mickael@lupusmic.org>
+ *  Copyright	2013, Lupus Michaelis
+ *  License	AGPL <http://www.fsf.org/licensing/licenses/agpl-3.0.html>
+ */
+
+/*
+ *  This file is part of Horn Framework.
+ *
+ *  Horn Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Horn Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero Public License
+ *  along with Horn Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace horn\lib ;
+use \horn\lib as h;
+
+h\import('lib/object') ;
+h\import('lib/collection') ;
+
+class decorator
+	extends object_public
+{
+	protected	$_next;
+
+	public		function __construct(decorator $next = null)
+	{
+		$this->_next = $next;
+		parent::__construct();
+	}
+
+	protected	function &_get_next()
+	{
+		if($this->has_next())
+			return $this->_next;
+
+		$this->_throw('There is next no more');
+	}
+
+	public		function has_next()
+	{
+		return !is_null($this->_next);
+	}
+}
+
