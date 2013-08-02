747219abc15314fa5dfc6b658a0b84086db6dc41
Mickaël Wolff <mickael@lupusmic.org>
2013-08-02 12:58:22 +0200

    Lib Add response helpers to feed it

--- lib/http/message.php
+++ lib/http/message.php
@@ -109,12 +109,12 @@ class response
 }
 
 class head
-	extends \horn\lib\collection
+	extends h\collection
 {
 }
 
 class body
-	extends \horn\lib\object_public
+	extends h\object_public
 {
 	public	$content;
 
@@ -123,5 +123,52 @@ class body
 	}
 }
 
+
+final
+class response_methods
+{
+	const			version = '1.1';
+
+	static public	function ok(response $response)
+	{
+		self::status($response, 200, 'OK');
+	}
+
+	static public	function no_content(response $response)
+	{
+		self::status($response, 204, 'Not found');
+	}
+
+	static public	function forbidden(response $response)
+	{
+		self::status($response, 403, 'Forbidden');
+	}
+
+	static public	function not_found(response $response)
+	{
+		self::status($response, 404, 'Not found');
+	}
+
+	static public	function method_not_allowed(response $response)
+	{
+		self::status($response, '405', 'Method Not Allowed');
+	}
+
+	static public	function http_conflict(response $response)
+	{
+		self::status($response, 409, 'Conflict');
+	}
+
+	static public	function status(response $response, $code, $message)
+	{
+		$response->status = sprintf('HTTP/%s %s %s'
+				, self::version, $code, $message);
+	}
+
+	static public	function location(response $response, url $url)
+	{
+		$response->head['Location'] = $url;
+	}
+}
 
 
